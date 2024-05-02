beef27d2ec63a5e07d987a7ff94fd72400c20be9
Renan Nunes <carlosrenannunes2003@outlook.com>
2024-05-02 17:06:13 -0300

    corrigindo erros mobile

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ menu.addEventListener("click", function (event) {
     //Adicionar no carrinho
     addToCart(name, price);
 
-    if (cart.length >= 4) {
+    if (cart.length >= 3) {
       cartItemsContainer.style.overflow = "auto";
       cartItemsContainer.style.maxHeight = "150px";
     }
